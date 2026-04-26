8fe610cb26156ad52ade36fc693b49525c6f3da5
Bluebird-j <julius.gradyjr@gmail.com>
2026-04-26 13:37:49 -0500

    Create restaurant menu a separate list of restaurants and their menu's to pull from.

---
+++ src/data/resturantMenu.ts
@@ -0,0 +1,253 @@
+const CANES_ITEMS = [
+  {
+    category: "Combos",
+    badge: "🔥 Most Popular",
+    name: "Box Combo",
+    desc: "4 Chicken Fingers, 1 Cane's Sauce, Crinkle Cut Fries, Texas Toast, 22oz Drink",
+    price: "$11.49",
+  },
+  {
+    category: "Combos",
+    name: "3 Finger Combo",
+    desc: "3 Chicken Fingers, 1 Cane's Sauce, Crinkle Cut Fries, Texas Toast, 22oz Drink",
+    price: "$9.59",
+  },
+  {
+    category: "Combos",
+    name: "Caniac Combo",
+    desc: "6 Chicken Fingers, 2 Cane's Sauces, Crinkle Cut Fries, Coleslaw, Texas Toast, Large Drink",
+    price: "$15.99",
+  },
+  {
+    category: "Combos",
+    name: "Sandwich Combo",
+    desc: "Chicken Finger Sandwich, Crinkle Cut Fries, 22oz Drink",
+    price: "$10.79",
+  },
+  {
+    category: "Combos",
+    name: "Kids Combo",
+    desc: "2 Chicken Fingers, 1 Cane's Sauce, Crinkle Cut Fries, Kids Drink",
+    price: "$6.49",
+  },
+  {
+    category: "TailGates",
+    badge: "🔥 Most Popular",
+    name: "25-finger Tailgate",
+    desc: "25 Chicken Fingers, 8 Cane's Sauces",
+    price:"$41.99",
+
+
+
+
+  },
+  {
+    category: "TailGates",
+    name: "50-finger Tailgate",
+    desc: "50 Chicken Fingers, 16 Cane's Sauces or 22 oz. cup",
+    price:"$79.99",
+  },
+  {
+    category: "TailGates",
+    name: "75-finger Tailgate",
+    desc: "75 Chicken Fingers, 25 Cane's Sauces or 32 oz. cup",
+    price:"$118.99",
+
+
+
+
+  },
+  {
+    category: "TailGates",
+    name: "100-finger Tailgate",
+    desc: "100 Chicken Fingers, 33 Cane's Sauces or (2) 22 oz. cups",
+    price:"$142.99",
+
+
+
+
+  },
+  {
+    category: "TailGates",
+    name: "200-finger Tailgate",
+    desc: "200 Chicken Fingers, 66 Cane's Sauces or (4) 22 oz cups",
+    price:"$281.98",
+
+
+
+
+  },
+  {
+    category: "TailGates",
+    name: "300-finger Tailgate",
+    desc: "300 Chicken Fingers, 99 Cane's Sauces or (6) 22 oz. cups",
+    price:"$420.97",
+  },
+  {
+    category: "TailGates",
+    name: "Crinkle-Cut Fries",
+    desc: "Crinkle-cut and perfectly salted",
+    price:"$2.49",
+
+
+
+
+  },
+  {
+    category: "TailGates",
+    name: "Cane's Sauce",
+    desc: "Secret recipe, made in-restaurant everyday",
+    price:"$0.39",
+
+
+
+
+  },
+  {
+    category: "TailGates",
+    name: "Texas Toast",
+    desc: "Golden brown and garlic buttery",
+    price:"$1.38",
+
+
+
+
+  },
+  {
+    category: "TailGates",
+    name: "Coleslaw",
+    desc: "Crisp, creamy and freshly prepared",
+    price:"$1.38",
+
+
+
+
+  },
+  {
+    category: "Extras",
+    name: "Chicken Finger",
+    desc: "All you need is 1",
+    price:"$1.99",
+
+
+
+
+  },
+ 
+  {
+    category: "Extras",
+    name: "Crinkle-Cut Fries",
+    desc: "Crinkle-cut and perfectly salted",
+    price:"$2.49",
+
+
+
+
+  },
+  {
+    category: "Extras",
+    badge:"Best-Seller",
+    name: "Cane's Sauce",
+    desc: "Secret recipe, made in-restaurant everyday",
+    price:"$0.39",
+
+
+
+
+  },
+  {
+    category: "Extras",
+    name: "Texas Toast",
+    desc: "Golden brown and garlic buttery",
+    price:"$1.38",
+
+
+
+
+  },
+  {
+    category: "Extras",
+    name: "Coleslaw",
+    desc: "Crisp, creamy and freshly prepared",
+    price:"$1.38",
+  },
+   {
+    category: "Extras",
+    name: "Sandwhich",
+    desc: "3 Chicken Fingers, Cane's Sauces and lettuce on a toasted bun",
+    price:"$7.69",
+  },
+
+
+
+
+  {
+    category: "Drinks",
+    badge:"Fan-Favorite",
+    name: "Lemonade",
+    desc: "Regular",
+    price:"$2.69",
+
+
+
+
+  },
+  {
+    category: "Drinks",
+    name: "Sweet Tea",
+    desc: "Regular",
+    price:"$2.49",
+  },
+  {
+    category: "Drinks",
+    name: "Fountain Drink",
+    desc: "Your choice of Assorted Coke products, Regular size.",
+    price:"$2.49",
+
+
+
+
+  },
+  {
+    category: "Drinks",
+    name: "Bottled Water",
+    desc: "High Quality H20",
+    price:"$2.49",
+
+
+
+
+  },
+  {
+    category: "Drinks",
+    name: "Jug Lemonade",
+    desc: "A Gallon of classic Lemonade",
+    price:"$10.89",
+
+
+
+
+  },
+  {
+    category: "Drinks",
+    name: "JUG of Sweet Tea",
+    desc: "As good as your Grandma's",
+    price:"$5.99",
+  },
+
+
+
+
+  {
+    category: "Kids",
+    badge: "🔥 Popular",
+    name: "The Kids Combo",
+    desc: "2 Chicken Fingers, 1 Cane's Sauce, Crinkle-Cut Fries and a Drink",
+    price:"$6.69",
+
+
+
+
+  },
+];
+
